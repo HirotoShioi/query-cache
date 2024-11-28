36c0214c3d7086f2c328ec507ae421682f9d38b2
Hiroto Shioi <shioihigg@gmail.com>
2024-11-28 13:53:38 +0900

    Fix package

--- examples/react/src/components/query-demo.tsx
+++ examples/react/src/components/query-demo.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { QueryCacheClient } from '@hirotoshioi/query-cache-client';
+import { QueryCacheClient } from 'query-cache-client';
 import { Button } from '@/components/ui/button';
 import {
   Card,
